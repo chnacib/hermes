68f106875a39e40d02540a932e87ff6962d63f13
delanovictor <delanovictor3@gmail.com>
2022-12-23 13:44:08 -0300

    fix: edge cases excel

--- src/common/excel.py
+++ src/common/excel.py
@@ -8,6 +8,7 @@ load_dotenv()
 proj_name = os.getenv('PROJ_NAME')
 region = os.getenv('AWS_REGION')
 output_folder = os.getenv('OUPUT_FOLDER', 'output/')
+ext = '.xlsx'
 
 
 def export_to_excel(excel_data: dict, service_name: str):
@@ -23,19 +24,24 @@ def export_to_excel(excel_data: dict, service_name: str):
             df[key] = df[key].apply(lambda a: pd.to_datetime(a).date())
 
     df.to_excel(
-        f'{output_folder}{service_name}-{proj_name}-{region}.xlsx', index=False)
+        f'{output_folder}{service_name}-{proj_name}-{region}{ext}', index=False)
 
 
 def join_files():
 
+    # Cria pasta de output
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
+
+    # Remove o arquivo gerado anteriormente, se existe
+    if os.path.exists(output_folder + proj_name + ext):
+        os.remove(output_folder + proj_name + ext)
 
     # Pega todos os arquivos dentro da pasta de output que possuem o nome do projeto contido no nome do arquivo.
     file_list = [file for file in os.listdir(
         output_folder) if proj_name in file]
 
-    writer = pd.ExcelWriter(output_folder + proj_name + '.xlsx')
+    writer = pd.ExcelWriter(output_folder + proj_name + ext)
 
     for file in file_list:
         service_name = file.split('-').pop(0)
@@ -43,3 +49,5 @@ def join_files():
         excel_file.to_excel(writer, sheet_name=service_name)
 
     writer.save()
+
+    print('Output file: ' + os.path.abspath(output_folder + proj_name + ext))
