3f31db52f4f418170487f4d7d39a114157c76be6
chnacib <carlos_henrique190@yahoo.com.br>
2022-12-23 14:35:48 -0300

    feat: security groups

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from src.services import s3, ami, ebs, ec2, iam, loadbalancer, rds, snapshot
+from src.services import s3, ami, ebs, ec2, iam, loadbalancer, rds, snapshot,sg_ec2,sg_rds
 from src.common import excel
 
 import argparse
 parser = argparse.ArgumentParser()
 
 # # -db DATABSE -u USERNAME -p PASSWORD -size 20
-parser.add_argument("-s3", action='store_true', help="Include S3")
-parser.add_argument("-ec2", action='store_true', help="Include EC2")
-parser.add_argument("-ami", action='store_true', help="Include AMI")
-parser.add_argument("-ebs", action='store_true', help="Include EBS")
-parser.add_argument("-iam", action='store_true', help="Include IAM")
-parser.add_argument("-loadbalancer", action='store_true',
+parser.add_argument("--sg-rds", action='store_true', help="Include RDS security group")
+parser.add_argument("--sg-ec2", action='store_true', help="Include EC2 security groups")
+parser.add_argument("--s3", action='store_true', help="Include S3")
+parser.add_argument("--ec2", action='store_true', help="Include EC2")
+parser.add_argument("--ami", action='store_true', help="Include AMI")
+parser.add_argument("--ebs", action='store_true', help="Include EBS")
+parser.add_argument("--iam", action='store_true', help="Include IAM")
+parser.add_argument("--loadbalancer", action='store_true',
                     help="Include LoadBalancer")
-parser.add_argument("-rds", action='store_true', help="Include RDS")
-parser.add_argument("-snapshot", action='store_true', help="Include Snapshot")
-parser.add_argument("-all", action='store_true', help="Include All")
-parser.add_argument("-single", action='store_true',
+parser.add_argument("--rds", action='store_true', help="Include RDS")
+parser.add_argument("--snapshot", action='store_true', help="Include Snapshot")
+parser.add_argument("--all", action='store_true', help="Include All")
+parser.add_argument("--single", action='store_true',
                     help="Export to a single file")
 
 args = vars(parser.parse_args())
@@ -43,6 +45,13 @@ if args['rds'] or args['all']:
 
 if args['snapshot'] or args['all']:
     snapshot.run()
+
+if args['sg_ec2'] or args['all']:
+    sg_ec2.run()
+
+if args['sg_rds'] or args ['all']:
+    sg_rds.run()
+
 
 if args['single']:
     excel.join_files()
