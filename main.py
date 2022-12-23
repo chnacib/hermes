1e6d4f14f5c8180c7ee182e9189affd2127150ea
delanovictor <delanovictor3@gmail.com>
2022-12-23 14:33:13 -0300

    fix args

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # # -db DATABSE -u USERNAME -p PASSWORD -size 20
-parser.add_argument("-s3", action='store_true', help="Include S3")
-parser.add_argument("-ec2", action='store_true', help="Include EC2")
-parser.add_argument("-ami", action='store_true', help="Include AMI")
-parser.add_argument("-ebs", action='store_true', help="Include EBS")
-parser.add_argument("-iam", action='store_true', help="Include IAM")
-parser.add_argument("-loadbalancer", action='store_true',
+parser.add_argument("--s3", action='store_true', help="Include S3")
+parser.add_argument("--ec2", action='store_true', help="Include EC2")
+parser.add_argument("--ami", action='store_true', help="Include AMI")
+parser.add_argument("--ebs", action='store_true', help="Include EBS")
+parser.add_argument("--iam", action='store_true', help="Include IAM")
+parser.add_argument("--loadbalancer", action='store_true',
                     help="Include LoadBalancer")
-parser.add_argument("-rds", action='store_true', help="Include RDS")
-parser.add_argument("-snapshot", action='store_true', help="Include Snapshot")
-parser.add_argument("-all", action='store_true', help="Include All")
-parser.add_argument("-single", action='store_true',
+parser.add_argument("--rds", action='store_true', help="Include RDS")
+parser.add_argument("--snapshot", action='store_true', help="Include Snapshot")
+parser.add_argument("-a", "--all", action='store_true', help="Include All")
+parser.add_argument("-j", "--join", action='store_true',
                     help="Export to a single file")
 
 args = vars(parser.parse_args())
@@ -44,5 +44,5 @@ if args['rds'] or args['all']:
 if args['snapshot'] or args['all']:
     snapshot.run()
 
-if args['single']:
+if args['join']:
     excel.join_files()
